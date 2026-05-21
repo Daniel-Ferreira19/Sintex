08633c8f87689dfce5f99e3be227120e1aff18c3
Victor Santo Barcelos <victor61673226@edu.df.senac.br>
2026-05-21 15:21:06 -0300

    :wqVictor: parte do cabeçalho e organização suave de cores

--- sintex/src/App.jsx
+++ sintex/src/App.jsx
@@ -1,8 +1,35 @@
+<<<<<<< HEAD
 import { Routes, Route } from "react-router";
 export default function App() {
   return (
      <Routes>
       <Route path="/" element={<App />} />
     </Routes>
+=======
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
+import Admin from "./Pages/Admin/Admin";
+import Client from "./Pages/Client/Client";
+import Menu from "./Componentes/Menu/Menu";
+import LayoutMenu from "./Pages/LayoutMenu/LayoutMenu";
+import PaginaDeErro from "./PaginaDeErro/paginaderro";
+export default function App() {
+  return (
+    <>
+      <Menu />
+      <Routes>
+        <Route path="/" element={<LayoutMenu />}>
+          <Route path="client" element={<Client />} />
+          <Route index element={<Home />} />
+        </Route>
+
+        <Route path="admin" element={<Admin />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<PaginaDeErro />} />
+      </Routes>
+
+    </>
+>>>>>>> cb53c9c (Victor: parte do cabeçalho e organização suave de cores)
   )
 }
--- sintex/src/main.jsx
+++ sintex/src/main.jsx
@@ -1,13 +1,25 @@
+<<<<<<< HEAD
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router";
+=======
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom";
+>>>>>>> cb53c9c (Victor: parte do cabeçalho e organização suave de cores)
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+<<<<<<< HEAD
   <StrictMode>
     <BrowseRouter>
       <App />
     </BrowseRouter>
   </StrictMode>,
 )
+=======
+  <BrowserRouter> 
+    <App />
+  </BrowserRouter>
+)
+>>>>>>> cb53c9c (Victor: parte do cabeçalho e organização suave de cores)
